fc617e936093a7a2b4f098ca3de995f8cb7ef6a1
danielosorio <vdosavh@gmail.com>
2016-05-24 11:43:36 +0100

    WithMarkings

--- ProcessData.py
+++ ProcessData.py
@@ -2,8 +2,54 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-import Tkinter as tk
+from Tkinter import *
+from PIL import Image, ImageTk
 import numpy as np
+
+# Main Window Definitions
+
+class Window(Frame):
+    def __init__(self,master = None):
+        Frame.__init__(self,master)
+        self.master = master
+        self.init_window()
+        self.LibPhysLogo()
+
+
+    def init_window(self):
+        self.master.title("Wii Balance Board Data Processor")
+
+        self.pack(fill = BOTH, expand = 1)
+
+        quitButton = Button(self, text = "Quit", command = self.client_exit)
+
+        quitButton.place(x=640, y=400)
+
+        menu = Menu(self.master)
+        self.master.config(menu = menu)
+
+        file = Menu(menu)
+
+        file.add_command(label= "Exit", command = self.client_exit)
+        menu.add_cascade(label = "File", menu = file)
+
+        edit = Menu(menu)
+        edit.add_command(label = "Undo")
+        menu.add_cascade(label = "Edit", menu = edit)
+
+
+    def client_exit(self):
+        exit()
+
+    def LibPhysLogo(self):
+        load = Image.open("Images/logo_libphys.png")
+        render = ImageTk.PhotoImage(load)
+        img = Label(self,image = render)
+        img.image = render
+        img.place(x=0,y=0)
+
+
+# Data Processing Fuctions
 
 def uploadData(data):
     TL = []
@@ -25,7 +71,17 @@ def uploadData(data):
     return [TL, TR, BL, BR, time]
 
 
-Data = open('Data/PreMarks/Raw_Mon_16:22:19')
 
-[TL, TR, BL, BR, time] = uploadData(Data)
 
+
+if __name__ == "__main__":
+
+    Data = open('Data/PreMarks/Raw_Mon_16:22:19')
+    [TL, TR, BL, BR, time] = uploadData(Data)
+
+
+    root = Tk()
+    root.geometry("640x400")
+
+    app = Window(root)
+    root.mainloop()
