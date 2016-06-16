e6ba02378f6e4af008d07c01ee9ae1ce5595167d
danielosorio <vdosavh@gmail.com>
2016-06-16 17:48:06 +0100

    PlotsWots

--- Calibration/calibration_main.py
+++ Calibration/calibration_main.py
@@ -57,7 +57,7 @@ def load_file(complete_raw_path, complete_converted_path, axes, title="Plots", s
     if show_plot:
         plot_show(figure)
 
-    return [rtl, rtr, rbl, rbr, rt, rtw, ctl, ctr, cbl, cbr, ct, ctw, figure]
+    return [rtl, rtr, rbl, rbr, rt, rtw, ctl, ctr, cbl, cbr, ct, ctw, figure, axis1, axis2, axis3]
 
 
 def load_12kg_corners(folder_path):
@@ -70,7 +70,7 @@ def load_12kg_corners(folder_path):
     [raw_12Kg_corner_dl_tl, raw_12Kg_corner_dl_tr, raw_12Kg_corner_dl_bl, raw_12Kg_corner_dl_br,
      raw_12Kg_corner_dl_time, raw_12Kg_corner_dl_tw, converted_12Kg_corner_dl_tl,
      converted_12Kg_corner_dl_tr, converted_12Kg_corner_dl_bl, converted_12Kg_corner_dl_br,
-     converted_12Kg_corner_dl_time, converted_12Kg_corner_dl_tw, dl_figure] = \
+     converted_12Kg_corner_dl_time, converted_12Kg_corner_dl_tw, dl_figure, ax9, ax8, ax7] = \
         load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax3, ax2, ax1],
                   title="Bottom Left Corner - 12Kg", show_plot=False)
 
@@ -78,26 +78,25 @@ def load_12kg_corners(folder_path):
     [raw_12Kg_corner_dr_tl, raw_12Kg_corner_dr_tr, raw_12Kg_corner_dr_bl, raw_12Kg_corner_dr_br,
      raw_12Kg_corner_dr_time, raw_12Kg_corner_dr_tw, converted_12Kg_corner_dr_tl,
      converted_12Kg_corner_dr_tr, converted_12Kg_corner_dr_bl, converted_12Kg_corner_dr_br,
-     converted_12Kg_corner_dr_time, converted_12Kg_corner_dr_tw, dr_figure] = \
-        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax3, ax2, ax1],
+     converted_12Kg_corner_dr_time, converted_12Kg_corner_dr_tw, dr_figure, ax9, ax8, ax7] = \
+        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax6, ax5, ax4],
                   title="Bottom Right Corner - 12Kg", show_plot=False)
 
     temp_end = 'ul.txt'
     [raw_12Kg_corner_ul_tl, raw_12Kg_corner_ul_tr, raw_12Kg_corner_ul_bl, raw_12Kg_corner_ul_br,
      raw_12Kg_corner_ul_time, raw_12Kg_corner_ul_tw, converted_12Kg_corner_ul_tl,
      converted_12Kg_corner_ul_tr, converted_12Kg_corner_ul_bl, converted_12Kg_corner_ul_br,
-     converted_12Kg_corner_ul_time, converted_12Kg_corner_ul_tw, ul_figure] = \
-        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax3, ax2, ax1],
+     converted_12Kg_corner_ul_time, converted_12Kg_corner_ul_tw, ul_figure, ax9, ax8, ax7] = \
+        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax9, ax8, ax7],
                   title="Upper Left Corner - 12Kg", show_plot=False)
 
     temp_end = 'ur.txt'
     [raw_12Kg_corner_ur_tl, raw_12Kg_corner_ur_tr, raw_12Kg_corner_ur_bl, raw_12Kg_corner_ur_br,
      raw_12Kg_corner_ur_time, raw_12Kg_corner_ur_tw, converted_12Kg_corner_ur_tl,
      converted_12Kg_corner_ur_tr, converted_12Kg_corner_ur_bl, converted_12Kg_corner_ur_br,
-     converted_12Kg_corner_ur_time, converted_12Kg_corner_ur_tw, ur_figure] = \
-        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax3, ax2, ax1],
+     converted_12Kg_corner_ur_time, converted_12Kg_corner_ur_tw, ur_figure, ax12, ax11, ax10] = \
+        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax12, ax11, ax10],
                   title="Upper Right Corner - 12Kg", show_plot=False)
-
 
 
     temp_data = [[converted_12Kg_corner_dl_time, converted_12Kg_corner_dr_time, converted_12Kg_corner_ul_time, converted_12Kg_corner_ur_time],
