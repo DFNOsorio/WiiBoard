24b3146f6a32ce3debd1604d5f955a9dc4933405
danielosorio <vdosavh@gmail.com>
2016-06-17 18:21:42 +0100

    PlotsWots

--- Calibration/calibration_main.py
+++ Calibration/calibration_main.py
@@ -46,65 +46,79 @@ def load_file(complete_raw_path, complete_converted_path, axes, title="Plots", s
 
     figure = subplot_overlap([rt, ct, ct], [[rtl, rtr, rbl, rbr], [ctl, ctr, cbl, cbr], [ctw]],
                              title=['Raw files', 'Converted files', 'Total weight'],
-                             xlabel=["Time (epochs)", "Time (s)", "Time (s)"],
+                             xlabel=["Time (s)", "Time (s)", "Time (s)"],
                              ylabel=["Raw values", "Converted Values (Kg)", 'Total weight (Kg)'],
                              legend=[["TOP_LEFT", "TOP_RIGHT", "BOTTOM_LEFT", "BOTTOM_RIGHT"],
-                                     ["TOP_LEFT", "TOP_RIGHT", "BOTTOM_LEFT", "BOTTOM_RIGHT", "TOTAL"],
+                                     ["TOP_LEFT", "TOP_RIGHT", "BOTTOM_LEFT", "BOTTOM_RIGHT"],
                                      ["TOTAL"]],
                              lines=1, columns=3, fontsize=12)
 
     figure = add_sup_title(figure, title, fontsize=14)
     if show_plot:
         plot_show(figure)
-    http: // nellev.github.io / tmp / jhepc / gallery.html
-    return [rtl, rtr, rbl, rbr, rt, rtw, ctl, ctr, cbl, cbr, ct, ctw, figure, axis1, axis2, axis3]
+
+    data1 = [rt, [rtl, rtr, rbl, rbr], "Time(s)", "Raw values",  "Raw " + title,
+             ["TOP_LEFT", "TOP_RIGHT", "BOTTOM_LEFT", "BOTTOM_RIGHT"]]
+    data2 = [ct, [ctl, ctr, cbl, cbr], "Time (s)", "Converted Values (Kg)", 'Converted files',
+             ["TOP_LEFT", "TOP_RIGHT", "BOTTOM_LEFT", "BOTTOM_RIGHT"]]
+
+    axis1 = axe_populator(data1, axes[0])
+    axis2 = axe_populator(data2, axes[1])
+    return [rtl, rtr, rbl, rbr, rt, rtw, ctl, ctr, cbl, cbr, ct, ctw, figure, axis1, axis2]
+
 
 
 def load_12kg_corners(folder_path):
     raw_files = '/raw/Raw_12Kg_corner_'
     converted_files = '/converted/Converted_12Kg_corner_'
 
-    f, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10, ax11, ax12, ax13 = grid()
+    f, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9 = grid()
 
     temp_end = 'dl.txt'
     [raw_12Kg_corner_dl_tl, raw_12Kg_corner_dl_tr, raw_12Kg_corner_dl_bl, raw_12Kg_corner_dl_br,
      raw_12Kg_corner_dl_time, raw_12Kg_corner_dl_tw, converted_12Kg_corner_dl_tl,
      converted_12Kg_corner_dl_tr, converted_12Kg_corner_dl_bl, converted_12Kg_corner_dl_br,
-     converted_12Kg_corner_dl_time, converted_12Kg_corner_dl_tw, dl_figure, ax9, ax8, ax7] = \
-        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax3, ax2, ax1],
+     converted_12Kg_corner_dl_time, converted_12Kg_corner_dl_tw, dl_figure, ax2, ax1] = \
+        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax2, ax1],
                   title="Bottom Left Corner - 12Kg", show_plot=False)
 
     temp_end = 'dr.txt'
     [raw_12Kg_corner_dr_tl, raw_12Kg_corner_dr_tr, raw_12Kg_corner_dr_bl, raw_12Kg_corner_dr_br,
      raw_12Kg_corner_dr_time, raw_12Kg_corner_dr_tw, converted_12Kg_corner_dr_tl,
      converted_12Kg_corner_dr_tr, converted_12Kg_corner_dr_bl, converted_12Kg_corner_dr_br,
-     converted_12Kg_corner_dr_time, converted_12Kg_corner_dr_tw, dr_figure, ax9, ax8, ax7] = \
-        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax6, ax5, ax4],
+     converted_12Kg_corner_dr_time, converted_12Kg_corner_dr_tw, dr_figure, ax4, ax3] = \
+        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax4, ax3],
                   title="Bottom Right Corner - 12Kg", show_plot=False)
 
     temp_end = 'ul.txt'
     [raw_12Kg_corner_ul_tl, raw_12Kg_corner_ul_tr, raw_12Kg_corner_ul_bl, raw_12Kg_corner_ul_br,
      raw_12Kg_corner_ul_time, raw_12Kg_corner_ul_tw, converted_12Kg_corner_ul_tl,
      converted_12Kg_corner_ul_tr, converted_12Kg_corner_ul_bl, converted_12Kg_corner_ul_br,
-     converted_12Kg_corner_ul_time, converted_12Kg_corner_ul_tw, ul_figure, ax9, ax8, ax7] = \
-        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax9, ax8, ax7],
+     converted_12Kg_corner_ul_time, converted_12Kg_corner_ul_tw, ul_figure, ax6, ax5] = \
+        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax6, ax5],
                   title="Upper Left Corner - 12Kg", show_plot=False)
 
     temp_end = 'ur.txt'
     [raw_12Kg_corner_ur_tl, raw_12Kg_corner_ur_tr, raw_12Kg_corner_ur_bl, raw_12Kg_corner_ur_br,
      raw_12Kg_corner_ur_time, raw_12Kg_corner_ur_tw, converted_12Kg_corner_ur_tl,
      converted_12Kg_corner_ur_tr, converted_12Kg_corner_ur_bl, converted_12Kg_corner_ur_br,
-     converted_12Kg_corner_ur_time, converted_12Kg_corner_ur_tw, ur_figure, ax12, ax11, ax10] = \
-        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax12, ax11, ax10],
+     converted_12Kg_corner_ur_time, converted_12Kg_corner_ur_tw, ur_figure, ax8, ax7] = \
+        load_file(folder_path + raw_files + temp_end, folder_path + converted_files + temp_end, [ax8, ax7],
                   title="Upper Right Corner - 12Kg", show_plot=False)
 
+    dl_max_mean = np.mean(converted_12Kg_corner_dl_tw[-200:])
+    dr_max_mean = np.mean(converted_12Kg_corner_dr_tw[-200:])
+    ul_max_mean = np.mean(converted_12Kg_corner_ul_tw[-200:])
+    ur_max_mean = np.mean(converted_12Kg_corner_ur_tw[-200:])
 
     temp_data = [[converted_12Kg_corner_dl_time, converted_12Kg_corner_dr_time, converted_12Kg_corner_ul_time, converted_12Kg_corner_ur_time],
                  [converted_12Kg_corner_dl_tw, converted_12Kg_corner_dr_tw, converted_12Kg_corner_ul_tw, converted_12Kg_corner_ur_tw],
-                 "Time(s)", "Total weight (Kg)",  "Total weight (Kg)", ["TOP_LEFT", "TOP_RIGHT", "BOTTOM_LEFT", "BOTTOM_RIGHT"],
-                 'TOP_LEFT max: \nTOP_RIGHT max: \nBOTTOM_LEFT max: \nBOTTOM_RIGHT max: ']
+                 "Time(s)", "Total weight (Kg)",  "Total weight (Kg)",
+                 ["BOTTOM_LEFT", "BOTTOM_RIGHT", "TOP_LEFT", "TOP_RIGHT"],
+                 'BOTTOM_LEFT max: ' + str(dl_max_mean) + ' Kg \nBOTTOM_RIGHT max: ' + str(dr_max_mean) +
+                 ' Kg \nTOP_LEFT max: ' + str(ul_max_mean) + ' Kg \nTOP_RIGHT max: ' + str(ur_max_mean) + ' Kg']
 
-    ax13 = ax13_populator(temp_data, ax13)
+    ax9 = ax13_populator(temp_data, ax9)
 
     plot_show_all()
 
--- Calibration/printing_lib.py
+++ Calibration/printing_lib.py
@@ -43,32 +43,30 @@ def grid():
 
     f = plt.figure()
 
-    gs1 = GridSpec(2, 3)
-    gs1.update(left=0.02, right=0.38, wspace=0.1)
+    gs1 = GridSpec(2, 2)
+    gs1.update(left=0.05, right=0.38, wspace=0.3, hspace=0.2)
 
     ax1 = plt.subplot(gs1[0, -1])
     ax2 = plt.subplot(gs1[0, -2])
-    ax3 = plt.subplot(gs1[0, -3])
-    ax4 = plt.subplot(gs1[-1, -1])
-    ax5 = plt.subplot(gs1[-1, -2])
-    ax6 = plt.subplot(gs1[-1, -3])
 
-    gs3 = GridSpec(2, 3)
-    gs3.update(left=0.41, right=0.78, wspace=0.1)
-    ax7 = plt.subplot(gs3[0, -1])
-    ax8 = plt.subplot(gs3[0, -2])
-    ax9 = plt.subplot(gs3[0, -3])
-    ax10 = plt.subplot(gs3[-1, -1])
-    ax11 = plt.subplot(gs3[-1, -2])
-    ax12 = plt.subplot(gs3[-1, -3])
+    ax3 = plt.subplot(gs1[-1, -1])
+    ax4 = plt.subplot(gs1[-1, -2])
+
+    gs3 = GridSpec(2, 2)
+    gs3.update(left=0.45, right=0.78, wspace=0.3, hspace=0.2)
+    ax5 = plt.subplot(gs3[0, -1])
+    ax6 = plt.subplot(gs3[0, -2])
+
+    ax7 = plt.subplot(gs3[-1, -1])
+    ax8 = plt.subplot(gs3[-1, -2])
 
     gs2 = GridSpec(2, 2)
     gs2.update(left=0.81, right=0.98, hspace=0.1)
-    ax13 = plt.subplot(gs2[:, :])
+    ax9 = plt.subplot(gs2[:, :])
 
     plt.subplots_adjust(hspace=0.1, top=0.95, bottom=0.04)
 
-    return f, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10, ax11, ax12, ax13
+    return f, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9
 
 
 def ax13_populator(filler, ax):
@@ -85,7 +83,7 @@ def ax13_populator(filler, ax):
     ax.set_ylabel(filler[3])
     ax.set_title(filler[4], fontsize=12)
     ax.legend(filler[5], fontsize=10)
-    ax.text(30, 8, filler[6],
+    ax.text(25, 8, filler[6],
             horizontalalignment='left',
             verticalalignment='center',
             bbox=dict(boxstyle="square",
@@ -98,16 +96,20 @@ def ax13_populator(filler, ax):
 
 
 def axe_populator(data, ax):
-    ax.plot(data[0], data[1])
+    x = data[0]
+    yy = data[1]
+
+    for j in range(0, len(yy)):
+        ax.plot(x, yy[j])
     for label in ax.get_xticklabels():
-        label.set_fontsize(4)
+        label.set_fontsize(10)
 
     for label in ax.get_yticklabels():
-        label.set_fontsize(4)
+        label.set_fontsize(10)
 
     ax.set_xlabel(data[2])
     ax.set_ylabel(data[3])
-    ax.set_title(data[4], fontsize=7)
+    ax.set_title(data[4], fontsize=12)
     ax.legend(data[5], fontsize=10)
     return ax
 
